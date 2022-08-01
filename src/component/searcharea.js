4cf06aff0a2991d4df393065aa66fb82bb70c305
Pradeep <pradeeplingasamy823@gmail.com>
2022-08-01 23:16:36 +0530

    m

--- src/component/searcharea.js
+++ src/component/searcharea.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Searcharea=({country:{country, count} , filt})=>{
     return(
+        
         <div className="searchbox">
             <button  onClick={()=>filt(country)}>{country}({count})</button>
         </div>
